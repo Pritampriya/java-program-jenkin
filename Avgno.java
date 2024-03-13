aa5fbb3ccf0ed01caa58f20b9f8b855c76da14ae
Pritam priyadarshi <priyadarshipritam752@gmail.com>
2024-03-13 19:05:19 +0530

    Initial commit

---
+++ Avgno.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class avgdigit
+{
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+ 		System.out.println("enter a no");
+		int n= sc.nextInt();
+		int digit;
+		int sum = 0;
+		while(n>0)
+		{
+		digit =n%10;
+		sum=sum+digit;
+		n=n/10;
+		}
+		double avg=sum/2.0;
+		System.out.println("avg is" + avg);
+	}
+}
